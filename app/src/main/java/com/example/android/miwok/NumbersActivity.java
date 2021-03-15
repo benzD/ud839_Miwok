1c90f27ff775929445a85cec8afdc71312f1d3e0
benzD <benz.dominic23@gmail.com>
2021-03-16 00:14:21 +0100

    practice

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,6 +3,10 @@ package com.example.android.miwok;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,5 +14,29 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> numberList = new ArrayList<String>();
+
+        numberList.add("one");
+        numberList.add("two");
+        numberList.add("three");
+        numberList.add("four");
+        numberList.add("five");
+        numberList.add("six");
+        numberList.add("seven");
+        numberList.add("eight");
+        numberList.add("nine");
+
+        LinearLayout numbersLayout = findViewById(R.id.numbers_root_view);
+
+        int index = 0;
+
+        while (index < numberList.size()){
+            TextView numberTextView = new TextView(this);
+            numberTextView.setText(numberList.get(index));
+            numbersLayout.addView(numberTextView);
+
+            index++;
+        }
     }
 }
